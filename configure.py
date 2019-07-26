40146f0f78a0e4cf78f4a2e2e5ec5a6ccb5f5f24
Abhishek <abhishekasdzxc@gmail.com>
2019-07-26 17:46:44 +0530

    Updated files

---
+++ configure.py
@@ -0,0 +1,14 @@
+options = {
+			"epoch": 0,
+			"nEpochs": 200,
+			"batchsize": 2,
+			"datapath": "./young2old",
+			"outputpath":'./output/',
+			"learningrate": 2e-4,
+			"decayEpoch": 50,
+			"inResolution":[256, 256, 3],
+			"outResolution":[256, 256, 3],
+			"continued": False,
+			"nThreads":10,
+			"unpaired":True,
+		   }
--- train.py
+++ train.py
@@ -2,188 +2,177 @@ import torch
 import argparse
 import itertools
 from PIL import Image
-from datasets import ImageDataset
+from datasets import AnBDataset
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from models import Generator, Discriminator
 from utils import ReplayBuffer, LambdaLR, Logger, weights_init_normal
 
+from configure import options #Edit this file to control the parameters once, without everytime pasting huge run command for every run.
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
-parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
-parser.add_argument('--batchSize', type=int, default=2, help='size of the batches')
-parser.add_argument('--dataroot', type=str, default='./young2old/', help='root directory of the dataset')
-parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
-parser.add_argument('--decay_epoch', type=int, default=50, help='epoch to start linearly decaying the learning rate to 0')
-parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
-parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
-parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
-parser.add_argument('--cuda', action='store_true', help='use GPU computation')
-parser.add_argument('--continued', action='store_true', help='helps continuing a broken training')
-parser.add_argument('--n_cpu', type=int, default=8, help='number of cpu threads to use during batch generation')
-opt = parser.parse_args()
-print(opt)
 
-if torch.cuda.is_available() and not opt.cuda:
-    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+inH, inW, inC = options["inResolution"]
+outH, outW, outC = options["outResolution"]
+print("HEYYY")
 
-###### Definition of variables ######
-# Networks
-netG_A2B = Generator(opt.input_nc, opt.output_nc)
-netG_B2A = Generator(opt.output_nc, opt.input_nc)
-netD_A = Discriminator(opt.input_nc)
-netD_B = Discriminator(opt.output_nc)
+if torch.cuda.is_available():
+	options["cuda"]=True
 
-if opt.cuda:
-    netG_A2B.cuda()
-    netG_B2A.cuda()
-    netD_A.cuda()
-    netD_B.cuda()
+def LoadData(path):
+	ApplyTransforms = [ transforms.Resize((int(inH*1.12), int(inW*1.12)), Image.BICUBIC),
+						transforms.RandomCrop((inH, inW)),
+						transforms.RandomHorizontalFlip(),
+						transforms.ToTensor(),
+						transforms.Normalize((1/2,)*3,(1/2,)*3)]
+	Data = AnBDataset(path, transforms_arg=ApplyTransforms, unpaired=options["unpaired"])
+	dataloader = DataLoader(Data, batch_size=options["batchsize"], shuffle=True, num_workers=options["nThreads"])
+	return dataloader
 
-netG_A2B.apply(weights_init_normal)
-netG_B2A.apply(weights_init_normal)
-netD_A.apply(weights_init_normal)
-netD_B.apply(weights_init_normal)
-if opt.continued:
-	print("continueing from a broken position")
-	netG_A2B.load_state_dict(torch.load("output/netG_A2B.pth"))
-	netG_B2A.load_state_dict(torch.load("output/netG_B2A.pth"))
-	netD_A.load_state_dict(torch.load("output/netD_A.pth"))
-	netD_B.load_state_dict(torch.load("output/netD_B.pth"))
-# Lossess
-criterion_GAN = torch.nn.MSELoss()
-criterion_cycle = torch.nn.L1Loss()
-criterion_identity = torch.nn.L1Loss()
 
-# Optimizers & LR schedulers
-optimizer_G = torch.optim.Adam(itertools.chain(netG_A2B.parameters(), netG_B2A.parameters()), lr=opt.lr, betas=(0.5, 0.999))
-optimizer_D_A = torch.optim.Adam(netD_A.parameters(), lr=opt.lr, betas=(0.5, 0.999))
-optimizer_D_B = torch.optim.Adam(netD_B.parameters(), lr=opt.lr, betas=(0.5, 0.999))
 
-lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
-lr_scheduler_D_A = torch.optim.lr_scheduler.LambdaLR(optimizer_D_A, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
-lr_scheduler_D_B = torch.optim.lr_scheduler.LambdaLR(optimizer_D_B, lr_lambda=LambdaLR(opt.n_epochs, opt.epoch, opt.decay_epoch).step)
+def trainer(options):
+	startEpoch = options["epoch"]
+	nEpochs = options["nEpochs"]
+	decayEpoch = options["decayEpoch"]
+	assert(nEpochs>decayEpoch), "The decay epoch is larger than total epochs, There will be no decay :P, Sure?"
+	#GAN to map from Generate A to B and Discriminate B
+	GEN_AtoB = Generator(inC, outC)
+	DIS_B = Discriminator(outC)
+	
+	#GAN to map from Generate B to A and Discriminate A
+	GEN_BtoA =  Generator(outC, inC)
+	DIS_A = Discriminator(inC)
+	if options["cuda"]:
+		GEN_AtoB.cuda()
+		DIS_B.cuda()
+		GEN_BtoA.cuda()
+		DIS_A.cuda()
+	GEN_AtoB.apply(weights_init_normal)
+	DIS_B.apply(weights_init_normal)
+	GEN_BtoA.apply(weights_init_normal)
+	DIS_A.apply(weights_init_normal)
+	if options["continued"]:
+		print("continuing from a broken position")
+		GEN_AtoB.load_state_dict(torch.load("output/GEN_AtoB.pth"))
+		DIS_B.load_state_dict(torch.load("output/DIS_B.pth"))
+		GEN_BtoA.load_state_dict(torch.load("output/GEN_BtoA.pth"))
+		DIS_A.load_state_dict(torch.load("output/DIS_A.pth"))
+	# LOSSES
+	GANLoss = torch.nn.MSELoss() # ImageA to ImageB distance
+	CycleLoss = torch.nn.L1Loss() # Absolute loss
+	IdentityLoss  = torch.nn.L1Loss() # ImageA->ImageB->ImageA' distance
 
-# Inputs & targets memory allocation
-Tensor = torch.cuda.FloatTensor if opt.cuda else torch.Tensor
-input_A = Tensor(opt.batchSize, opt.input_nc, opt.size, opt.size)
-input_B = Tensor(opt.batchSize, opt.output_nc, opt.size, opt.size)
-target_real = Variable(Tensor(opt.batchSize).fill_(1.0), requires_grad=False)
-target_fake = Variable(Tensor(opt.batchSize).fill_(0.0), requires_grad=False)
+	optim_GAN = torch.optim.Adam(list(GEN_AtoB.parameters())+list(GEN_BtoA.parameters()), lr=options["learningrate"], betas=[0.5, 0.999])
+	optim_DIS_A = torch.optim.Adam(DIS_A.parameters(), lr=options["learningrate"], betas=[0.5, 0.999])
+	optim_DIS_B = torch.optim.Adam(DIS_B.parameters(), lr=options["learningrate"], betas=[0.5, 0.999])
 
-fake_A_buffer = ReplayBuffer()
-fake_B_buffer = ReplayBuffer()
+	# LR Scheduler should be here
+	lr_scheduler = lambda ep: 1.0 - max(0, ep+startEpoch)/(nEpochs - decayEpoch)
 
-# Dataset loader
-transforms_ = [ transforms.Resize(int(opt.size*1.12), Image.BICUBIC), 
-                transforms.RandomCrop(opt.size), 
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ]
-DATATA=ImageDataset(opt.dataroot, transforms_=transforms_, unaligned=True)
-print(opt.batchSize,opt.n_cpu,len(DATATA),opt.dataroot)
-dataloader = DataLoader(DATATA, batch_size=opt.batchSize, shuffle=True, num_workers=opt.n_cpu)
+	lr_scheduler_GAN = torch.optim.lr_scheduler.LambdaLR(optim_GAN, lr_lambda=lr_scheduler)
+	lr_scheduler_Dis_A = torch.optim.lr_scheduler.LambdaLR(optim_DIS_A, lr_lambda=lr_scheduler)
+	lr_scheduler_Dis_B = torch.optim.lr_scheduler.LambdaLR(optim_DIS_B, lr_lambda=lr_scheduler)
 
-# Loss plot
-logger = Logger(opt.n_epochs, len(dataloader))
-###################################
+	# Tensors Memory Allocation
+	batchsize = options["batchsize"]
+	if options["cuda"]:
+		print("CUDA-fied")
+		Tensor = torch.cuda.FloatTensor
+	else:
+		Tensor = torch.Tensor
+	imageA = Tensor(batchsize, inC, inH, inW)
+	imageB = Tensor(batchsize, outC, outH, outW)
+	targetReal = Variable(Tensor(batchsize).fill_(1.0), requires_grad=False)
+	targetFake = Variable(Tensor(batchsize).fill_(0.0), requires_grad=False)
 
-###### Training ######
-for epoch in range(opt.epoch, opt.n_epochs):
-    for i, batch in enumerate(dataloader):
-        # Set model input
-        real_A = Variable(input_A.copy_(batch['A']))
-        real_B = Variable(input_B.copy_(batch['B']))
+	FakeAHolder = ReplayBuffer() # Check this Check this
+	FakeBHolder = ReplayBuffer()
 
-        ###### Generators A2B and B2A ######
-        optimizer_G.zero_grad()
+	dataloader = LoadData(options["datapath"])
+	logger = Logger(nEpochs, len(dataloader))
+	#Actual Training
+	for epoch in range(startEpoch, nEpochs):
+		for batch_id, batch_data in enumerate(dataloader):
+			realA = Variable(imageA.copy_(batch_data['A']))
+			realB = Variable(imageB.copy_(batch_data['B']))
 
-        # Identity loss
-        # G_A2B(B) should equal B if real B is fed
-        same_B = netG_A2B(real_B)
-        loss_identity_B = criterion_identity(same_B, real_B)*5.0
-        # G_B2A(A) should equal A if real A is fed
-        same_A = netG_B2A(real_A)
-        loss_identity_A = criterion_identity(same_A, real_A)*5.0
+			optim_GAN.zero_grad()
 
-        # GAN loss
-        fake_B = netG_A2B(real_A)
-        pred_fake = netD_B(fake_B)
-        loss_GAN_A2B = criterion_GAN(pred_fake, target_real)
+			synthB = GEN_AtoB(realB)
+			lossIden_BtoB = IdentityLoss(synthB, realB)*5.0
 
-        fake_A = netG_B2A(real_B)
-        pred_fake = netD_A(fake_A)
-        loss_GAN_B2A = criterion_GAN(pred_fake, target_real)
+			synthA = GEN_BtoA(realA)
+			lossIden_AtoA = IdentityLoss(synthA, realA)*5.0
 
-        # Cycle loss
-        recovered_A = netG_B2A(fake_B)
-        loss_cycle_ABA = criterion_cycle(recovered_A, real_A)*10.0
+			fakeB = GEN_AtoB(realA)
+			classB = DIS_B(fakeB)
+			lossGEN_A2B = GANLoss(classB, targetReal)
 
-        recovered_B = netG_A2B(fake_A)
-        loss_cycle_BAB = criterion_cycle(recovered_B, real_B)*10.0
+			fakeA = GEN_BtoA(realB)
+			classA = DIS_A(fakeA)
+			lossGEN_B2A = GANLoss(classA, targetReal)
 
-        # Total loss
-        loss_G = loss_identity_A + loss_identity_B + loss_GAN_A2B + loss_GAN_B2A + loss_cycle_ABA + loss_cycle_BAB
-        loss_G.backward()
-        
-        optimizer_G.step()
-        ###################################
+			reconA = GEN_BtoA(fakeB)
+			lossCycle_ABA = CycleLoss(reconA, realA)*10.0
 
-        ###### Discriminator A ######
-        optimizer_D_A.zero_grad()
+			reconB = GEN_BtoA(fakeA)
+			lossCycle_BAB = CycleLoss(reconB, realB)*10.0
 
-        # Real loss
-        pred_real = netD_A(real_A)
-        loss_D_real = criterion_GAN(pred_real, target_real)
+			lossTotal = lossCycle_BAB + lossCycle_ABA + lossGEN_B2A + lossGEN_A2B + lossIden_AtoA + lossIden_BtoB
+			lossTotal.backward()
 
-        # Fake loss
-        fake_A = fake_A_buffer.push_and_pop(fake_A)
-        pred_fake = netD_A(fake_A.detach())
-        loss_D_fake = criterion_GAN(pred_fake, target_fake)
+			optim_GAN.step()
+			# Discriminator A
+			optim_DIS_A.zero_grad()
 
-        # Total loss
-        loss_D_A = (loss_D_real + loss_D_fake)*0.5
-        loss_D_A.backward()
+			classAreal = DIS_A(realA)
+			lossDreal = GANLoss(classAreal, targetReal)
 
-        optimizer_D_A.step()
-        ###################################
+			fakeA = FakeAHolder.push_and_pop(fakeA)
+			classAfake = DIS_A(fakeA.detach())
+			lossDfake = GANLoss(classAfake, targetFake)
 
-        ###### Discriminator B ######
-        optimizer_D_B.zero_grad()
+			lossDA = (lossDreal + lossDfake)/2
 
-        # Real loss
-        pred_real = netD_B(real_B)
-        loss_D_real = criterion_GAN(pred_real, target_real)
-        
-        # Fake loss
-        fake_B = fake_B_buffer.push_and_pop(fake_B)
-        pred_fake = netD_B(fake_B.detach())
-        loss_D_fake = criterion_GAN(pred_fake, target_fake)
+			lossDA.backward()
 
-        # Total loss
-        loss_D_B = (loss_D_real + loss_D_fake)*0.5
-        loss_D_B.backward()
+			optim_DIS_A.step()
 
-        optimizer_D_B.step()
-        ###################################
+			# Discriminator B
+			optim_DIS_B.zero_grad()
 
-        # Progress report (http://localhost:8097)
-        logger.log({'loss_G': loss_G, 'loss_G_identity': (loss_identity_A + loss_identity_B), 'loss_G_GAN': (loss_GAN_A2B + loss_GAN_B2A),
-                    'loss_G_cycle': (loss_cycle_ABA + loss_cycle_BAB), 'loss_D': (loss_D_A + loss_D_B)}, 
-                    images={'real_A': real_A, 'real_B': real_B, 'fake_A': fake_A, 'fake_B': fake_B})
+			classBreal = DIS_B(realB)
+			lossDreal = GANLoss(classBreal, targetReal)
 
-    # Update learning rates
-    lr_scheduler_G.step()
-    lr_scheduler_D_A.step()
-    lr_scheduler_D_B.step()
+			fakeB = FakeBHolder.push_and_pop(fakeB)
+			classBfake = DIS_B(fakeB.detach())
+			lossDfake = GANLoss(classBfake, targetFake)
+			lossDB = (lossDreal + lossDfake)/2
 
-    # Save models checkpoints
-    torch.save(netG_A2B.state_dict(), 'output/netG_A2B.pth')
-    torch.save(netG_B2A.state_dict(), 'output/netG_B2A.pth')
-    torch.save(netD_A.state_dict(), 'output/netD_A.pth')
-    torch.save(netD_B.state_dict(), 'output/netD_B.pth')
-    with open("EpochVerify.txt",'w') as ffff:
-    	ffff.write("\n"+str(epoch))
-###################################
+			lossDB.backward()
+
+			optim_DIS_B.step()
+			# Progress report (http://localhost:8097)
+			logger.log({'lossTotal': lossTotal, 'lossIdentity': (lossIden_AtoA + lossIden_BtoB), 'lossGAN': (lossGEN_A2B + lossGEN_B2A),
+					'lossCycle': (lossCycle_ABA + lossCycle_BAB), 'lossD': (lossDA + lossDB)}, 
+					images={'realA': realA, 'realB': realB, 'fakeA': fakeA, 'fakeB': fakeB})
+
+		#Update learning rates
+		lr_scheduler_GAN.step()
+		lr_scheduler_Dis_A.step()
+		lr_scheduler_Dis_B.step()
+
+		# Save Models checkpoints
+		torch.save(GEN_AtoB.state_dict(), options["outputpath"]+'GEN_AtoB.pth')
+		torch.save(DIS_B.state_dict(), options["outputpath"]+'DIS_B.pth')
+		torch.save(GEN_BtoA.state_dict(), options["outputpath"]+'GEN_BtoA.pth')
+		torch.save(DIS_A.state_dict(), options["outputpath"]+'DIS_A.pth')
+		with open("EpochVerify.txt",'w') as ff:
+			ff.write("\n"+str(epoch))
+
+
+
+if __name__=="__main__":
+	print("Starting the training")
+	trainer(options)
